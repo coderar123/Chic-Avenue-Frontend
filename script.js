1876f6be681e7c43e077d2002e005f44bb82603b
coderar123 <143030080+coderar123@users.noreply.github.com>
2024-10-20 12:28:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.getElementById('sideMenu');
+    menu.classList.toggle('show');
+}
+function toggleMenu1() {
+    // alert("button clicked");
+    const menu = document.getElementById('sideMenu1');
+    menu.classList.toggle('active'); // Use 'active' class for the visible state
+}
+
